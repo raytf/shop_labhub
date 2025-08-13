8fbffb02d56c48f0c5fd9785691b511f4e071b01
Ray Feng <ray@dotdothorse.com>
2025-08-13 16:11:51 +1000

    Returned baseUrl vercel env

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,6 +1,6 @@
 export const getBaseUrl = () => {
   if (process.env.VERCEL_ENV === "production") {
-    return process.env.VERCEL_PROJECT_PRODUCTION_URL;
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
   if (process.env.VERCEL_ENV === "preview") {
     return `https://${process.env.VERCEL_URL}`;
